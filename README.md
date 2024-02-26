e7f18526bcc9bda73221a769c71d2f0d4e167a8e
Shinrael <mircoelena89@gmail.com>
2024-02-26 15:47:22 +0100

    [add] milestone 2

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ js-array-carousel
 Dato un array contenente una lista di cinque immagini, creare un carosello.
 ## Svolgimento:
 1. Creo un array con tutte le immagini
-2. Dò di base la classe .hidden a tutte le immagini e lo tolgo subito solo alla prima immagine
+2. Dò di base la classe .hidden a tutte le immagini tramite un ciclo e lo tolgo subito solo alla prima immagine
--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,5 @@
+// Creo l'array con tutte le immagini
+
 const images = [
   'assets/img/01.webp',
   'assets/img/02.webp',
@@ -6,4 +8,23 @@ const images = [
   'assets/img/05.webp',
 ]
 
+// Creo la const con il contenitore delle immagini
+
 const imageContainer = document.querySelector('.image-container');
+
+// Avvio il ciclo per aggiungere la classe alle immagini
+
+for (let i = 0; i < images.length; i++) {
+  const img = images[i];
+  imageContainer.innerHTML += `<img class="image hidden" src="${img}">`
+}
+
+// Prendiamo tutte le immagini
+
+const imgCollection = document.getElementsByClassName('image');
+
+
+// Rimuoviamo la classe hidden alla prima immagine
+
+
+imgCollection[0].classList.remove('hidden')
